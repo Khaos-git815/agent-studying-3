1ef949459c9a6391c375315dabb994eaec254140
Khoas <coaljwaiter@gmail.com>
2025-07-13 23:56:13 -0700

    updated utils.py

--- utils.py
+++ utils.py
@@ -2,3 +2,18 @@ import random
 from collections import deque
 import numpy as np
 
+
+class ReplayBuffer:
+    def __init__(self, capacity=10000):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size=64):
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = zip(*batch)
+        return np.array(state), action, reward, np.array(next_state), done
+
+    def __len__(self):
+        return len(self.buffer)
